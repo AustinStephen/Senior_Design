83889df2372af246a4c6e6ba7bb1c0bf4880aa04
areiche1 <areiche1@uwyo.edu>
2023-04-14 09:22:01 -0600

    Changed path to model.json for amplify

--- src/src/app/mountain-classifier-form/mountain-classifier-form.component.ts
+++ src/src/app/mountain-classifier-form/mountain-classifier-form.component.ts
@@ -68,7 +68,7 @@ export class MountainClassifierFormComponent implements OnInit {
     this.initMap();
     // const modelUrl = '/assets/model-data/model.json';
     // this.model = await tf.loadLayersModel(tf.io.http(modelUrl));
-    this.model = await tf.loadLayersModel('/assets/model-data/model.json');
+    this.model = await tf.loadLayersModel('../../assets/model-data/model.json');
   }
 
   private initMap(): void {
