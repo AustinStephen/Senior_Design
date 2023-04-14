bb9fcb88dae823ea4016500b63481a6c4b5bf9d6
areiche1 <areiche1@uwyo.edu>
2023-04-14 11:33:54 -0600

    Trying to find model.json on amplify

--- src/src/app/mountain-classifier-form/mountain-classifier-form.component.ts
+++ src/src/app/mountain-classifier-form/mountain-classifier-form.component.ts
@@ -66,9 +66,10 @@ export class MountainClassifierFormComponent implements OnInit {
 
   async ngOnInit(): Promise<void> {
     this.initMap();
-    const modelUrl = '../../assets/model-data/model.json';
-    this.model = await tf.loadLayersModel(tf.io.http(modelUrl));
+    // const modelUrl = '/assets/model-data/model.json';
+    // this.model = await tf.loadLayersModel(tf.io.http(modelUrl));
     // this.model = await tf.loadLayersModel('/assets/model-data/model.json');
+    this.model = await tf.loadLayersModel('https://main.d2pr9vfn2vnfra.amplifyapp.com/assets/model-data/model.json');
   }
 
   private initMap(): void {
